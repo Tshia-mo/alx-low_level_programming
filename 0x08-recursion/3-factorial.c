7a5de177c642429a2f033c010d655ee3026c62f5
Tshiamo <ramongtshia@gmail.com>
2023-03-11 10:17:38 -0800

    3. task

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,15 +2,16 @@
 /**
  * _factorial - entry point
  * @n: input
+ *
  * Return: integer value
  */
 int factorial(int n)
 {
 	if (n < 0)
-     	  return(-1);
+	  return (-1);
 
 	if (n <= 1)
-	  return(-1);
+	  return (1);
 
-	return (n * factorial(n-1));
+	return (n * factorial(n - 1));
 }
