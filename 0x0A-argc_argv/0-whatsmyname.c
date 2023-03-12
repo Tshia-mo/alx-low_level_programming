63aca0a061f86713f2d1f2ebf076afebf47d81e3
Tshiamo <ramongtshia@gmail.com>
2023-03-12 12:13:33 -0700

    whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: input
+ * @argv: input
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
