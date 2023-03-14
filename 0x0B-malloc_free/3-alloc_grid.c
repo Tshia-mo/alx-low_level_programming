63161b63fc7d26dd0c58f2100fe48a5a6e8d8722
Tshiamo <ramongtshia@gmail.com>
2023-03-14 13:23:11 -0700

    malloc3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,12 +2,12 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * print_grit - 2 dimentional arrays to integers
+ * alloc_grid - 2 dimentional arrays to integers
  * @width: 2 dimention array
  * @height: 2 dimention array
  * Return: Always success 0.
  */
-int **alloc_grid(int width, int height);
+int **alloc_grid(int width, int height)
 {
 	int **grid, i, j;
 
