833610c3dcbb4c08cc04093f704df7d909fc8ba1
Tshia-mo <ramongtshia@gmail.com>
2023-04-09 05:58:43 -0700

    0.0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * file name: main.h
+ *
+ * Decription: prototypes of all the tasks
+ */
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
