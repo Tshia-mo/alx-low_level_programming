381dfe60114e90d279dc36554be8f968cb574923
Tshiamo <ramongtshia@gmail.com>
2023-05-08 18:13:20 -0700

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -11,25 +10,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int you;
-	ssize_t me, swe;
-	char *buffer;
+	char *buf;
+	ssize_t me;
+	ssize_t good;
+	ssize_t real;
 
-	if (!filename)
+	me = open(filename, O_RDONLY);
+	if (me == -1)
 		return (0);
-	you = open(filename, O_RDONLY);
-	if (you == -1)
-		return (0);
-	buffer = malloc(sizeof(char) * (letters));
-	if (!buffer)
-		return (0);
+	buf = malloc(sizeof(char) * letters);
+	real = read(me, buf, letters);
+	good = write(STDOUT_FILENO, buf, real);
 
-	me = read(you, buffer, letters);
-	swe = write(STDOUT_FILENO, buffer, me);
-
-	close(you);
-
-	free(buffer);
-
-	return (swe);
+	free(buf);
+	close(me);
+	return (good);
 }
