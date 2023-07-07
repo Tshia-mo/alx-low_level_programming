10e29df013a6786e24bba41ca4dd8a169e94b3a4
Tshiamo <ramongtshia@gmail.com>
2023-07-06 17:07:03 -0700

    Project-complete

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,15 +8,7 @@
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
-	{
 		print_binary(n >> 1);
-	}
-	if (n & 1)
-	{
-		putchar('1');
-	}
-	else
-	{
-		putchar('0');
-	}
+
+	_putchar((n & 1) + '0');
 }
